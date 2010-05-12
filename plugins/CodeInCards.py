3ccb2dde693635264870d7923f6cca48efdba606
zak <zak@7e13492b-593b-4012-89ab-55693cb872a0>
2010-05-12 04:59:40 +0000

    licence

--- plugins/CodeInCards.py
+++ plugins/CodeInCards.py
@@ -1,5 +1,11 @@
 '''
-CodeInCards - a plugin to embed Python code in cards/card templates
+CodeInCards - an Anki plugin to embed Python code in cards/card templates
+
+Copyright 2010 Isaac Wilcox
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
  
 Loads substition "libraries" from %pluginsFolder%/CodeInCards/*.py
 then provides syntax to let you substitute library calls into cards.
