2996aa69c64da680cab046b4e6f795e37737540b
zak <zak@7e13492b-593b-4012-89ab-55693cb872a0>
2010-05-15 13:40:48 +0000

    Use CIC.QorA now, not QorA, to match CodeInCards.py change.
    DocStringify.
    
    Add $Id$

--- plugins/CodeInCards/default.py
+++ plugins/CodeInCards/default.py
@@ -1,33 +1,38 @@
-# An example library for the CodeInCards plugin for Anki.
-#
-# Functions:
-#
-#   a(text)
-#     Applies a format to the text if it appears on the answer side of the card.
-#
-#   ifnem(text, fmt)
-#     Shows formatted text only if raw text is not empty.
-#     Use "%s" inside the format to substitute text.  In card templates you'll
-#     need to escape this as "%%s".
-#
-#   ifa(text)
-#     Shows text only if part of the answer side of the card.
-#
-#   ifq(text)
-#     Shows text only if part of the question side of the card.
-#
-#   pick(text)
-#     Splits text on "," to make a list and returns a random element.
-#     e.g. pick("any,of,these,terms,perhaps this one with spaces")
-#
-import random
+'''
+An example library for the CodeInCards plugin for Anki.
 
-# FIXME: should have a plugin to load a stylesheet from a file:
-#ANSWER_FORMAT = """<span class='answerHighlight'>%s</span>"""
-ANSWER_FORMAT = """<span style='color:blue; font-weight:bold; '>%s</span>"""
+Functions:
+
+  a(text)
+    Applies a format to the text if it appears on the answer side of the card.
+
+  ifnem(text, fmt)
+    Shows formatted text only if raw text is not empty.
+    Use "%s" inside the format to substitute text.  In card templates you'll
+    need to escape this as "%%s".
+
+  ifa(text)
+    Shows text only if part of the answer side of the card.
+
+  ifq(text)
+    Shows text only if part of the question side of the card.
+
+  pick(text)
+    Splits text on "," to make a list and returns a random element.
+    e.g. pick("any,of,these,terms,perhaps this one with spaces")
+
+$Id$
+'''
+import random
+import CodeInCards as CIC
+
+# FIXME: would be better if we could create the corresponding style in
+# %configDir%/style.css but that firstly doesn't work and secondly messes up
+# the whole UI. 
+ANSWER_FORMAT = """<span style='color:blue; font-weight:bold;'>%s</span>"""
 
 def a(text):
-        if QorA == "a":
+        if CIC.QorA == "a":
                 return ANSWER_FORMAT % text
         return text 
 
@@ -37,12 +42,12 @@ def ifnem(text, fmt):
         return ""
 
 def ifa(text):
-        if QorA == "a":
+        if CIC.QorA == "a":
                 return text
         return ""
 
 def ifq(text):
-        if QorA == "q":
+        if CIC.QorA == "q":
                 return text
         return ""
 
