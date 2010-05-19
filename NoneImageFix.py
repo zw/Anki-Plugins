93acd3b04b0989fc57ac3f07c05c9f6a35821dab
Zak Wilcox <zakwilcox+ankigithub@nospamplsthx.gmail.com>
2010-05-19 13:48:38 +0100

    Justify existence.

--- NoneImageFix.py
+++ NoneImageFix.py
@@ -1,3 +1,6 @@
+# Hack around the bug fixed in this commit:
+#   http://github.com/zw/ankiqt/commit/3e739e769878d1e41feef2e2428239465481d6d4
+# Should have used the wrap stuff for this.  Or even just run from source.
 import ankiqt.ui.facteditor
 import ankiqt.ui.utils
 from anki.lang import _
