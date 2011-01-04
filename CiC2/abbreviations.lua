ef1d03b114babec0c0bcfa7f0190f7a46d7f2bae
Zak Wilcox <zakwilcox+ankigithub@nospamplsthx.gmail.com>
2011-01-04 15:51:12 +0000

    Multiple additions + a bit more organisation.

--- CiC2/abbreviations.lua
+++ CiC2/abbreviations.lua
@@ -1,20 +1,56 @@
+-- Cardinality
+ST = "<span style='vertical-align:super; font-size:small; '>st</span>"
+ND = "<span style='vertical-align:super; font-size:small; '>nd</span>"
+RD = "<span style='vertical-align:super; font-size:small; '>rd</span>"
+TH = "<span style='vertical-align:super; font-size:small; '>th</span>"
+
+-- Roman numerals
+I    = "&#x2160;"
+II   = "&#x2161;"
+III  = "&#x2162;"
+IV   = "&#x2163;"
+V    = "&#x2164;"
+VI   = "&#x2165;"
+VII  = "&#x2166;"
+VIII = "&#x2167;"
+IX   = "&#x2168;"
+X    = "&#x2169;"
+XI   = "&#x216a;"
+XII  = "&#x216b;"
+
+-- Ions
 NA = "Na<span style='vertical-align:super; font-size:small; '>+</span>"
 CA = "Ca<span style='vertical-align:super; font-size:small; '>2+</span>"
+CL = "Cl<span style='vertical-align:super; font-size:small; '>-</span>"
 NCX = NA .. "/" .. CA
 K = "K<span style='vertical-align:super; font-size:small; '>+</span>"
+H = "H<span style='vertical-align:super; font-size:small; '>+</span>"
+HCO3 = "HCO<span style='vertical-align:sub; font-size:small; '>3</span><span style='vertical-align:super; font-size:small; '>-</span>"
+
+-- Pharmacology
 KD = "K<span style='vertical-align:sub; font-size:small; '>D</span>"
 EC50 = "EC<span style='vertical-align:sub; font-size:small; '>50</span>"
 LD50 = "LD<span style='vertical-align:sub; font-size:small; '>50</span>"
-H = "H<span style='vertical-align:super; font-size:small; '>+</span>"
+
+-- Compounds
 H3O = "H<span style='vertical-align:sub; font-size:small; '>3</span>O<span style='vertical-align:super; font-size:small; '>+</span>"
 H2O = "H<span style='vertical-align:sub; font-size:small; '>2</span>O"
 CO2 = "CO<span style='vertical-align:sub; font-size:small; '>2</span>"
 O2 = "O<span style='vertical-align:sub; font-size:small; '>2</span>"
+N2 = "N<span style='vertical-align:sub; font-size:small; '>2</span>"
+NADP = "NADP<span style='vertical-align:super; font-size:small; '>+</span>"
+
+-- A "P" in a circle, denoting phosphorylation
+PPN = "&#x024c5;" 
+
+-- ??
 LP = "L<span style='vertical-align:sub; font-size:small; '>p</span>"
 
+-- Immunology
 CD8 = "CD8<span style='vertical-align:super; font-size:small; '>+</span>"
 CD4 = "CD4<span style='vertical-align:super; font-size:small; '>+</span>"
 
+-- Eicosanoids
 PGG2 = "PGG<span style='vertical-align:sub; font-size:small; '>2</span>"
 PGH2 = "PGH<span style='vertical-align:sub; font-size:small; '>2</span>"
 PGI2 = "PGI<span style='vertical-align:sub; font-size:small; '>2</span>"
@@ -22,6 +58,7 @@ PGI3 = "PGI<span style='vertical-align:sub; font-size:small; '>3</span>"
 TXA2 = "TXA<span style='vertical-align:sub; font-size:small; '>2</span>"
 TXA3 = "TXA<span style='vertical-align:sub; font-size:small; '>3</span>"
 
+-- Receptors
 A1 = "&alpha;<span style='vertical-align:sub; font-size:small; '>1</span>"
 A2 = "&alpha;<span style='vertical-align:sub; font-size:small; '>2</span>"
 B1 = "&beta;<span style='vertical-align:sub; font-size:small; '>1</span>"
@@ -29,37 +66,41 @@ B2 = "&beta;<span style='vertical-align:sub; font-size:small; '>2</span>"
 M1 = "M<span style='vertical-align:sub; font-size:small; '>1</span>"
 M2 = "M<span style='vertical-align:sub; font-size:small; '>2</span>"
 M3 = "M<span style='vertical-align:sub; font-size:small; '>3</span>"
-
-ST = "<span style='vertical-align:super; font-size:small; '>st</span>"
-ND = "<span style='vertical-align:super; font-size:small; '>nd</span>"
-RD = "<span style='vertical-align:super; font-size:small; '>rd</span>"
-TH = "<span style='vertical-align:super; font-size:small; '>th</span>"
-
+M4 = "M<span style='vertical-align:sub; font-size:small; '>3</span>"
+M5 = "M<span style='vertical-align:sub; font-size:small; '>3</span>"
 G0 = "G<span style='vertical-align:sub; font-size:small; '>0</span>"
 G1 = "G<span style='vertical-align:sub; font-size:small; '>1</span>"
 G2 = "G<span style='vertical-align:sub; font-size:small; '>2</span>"
 GAI = "G<span style='vertical-align:sub; font-size:small; '>&alpha;i</span>"
 GAQ = "G<span style='vertical-align:sub; font-size:small; '>&alpha;q</span>"
-IP3R = "IP<span style='vertical-align:sub; font-size:small; '>3</span>R"
 GAS = "G<span style='vertical-align:sub; font-size:small; '>&alpha;s</span>"
+IP3R = "IP<span style='vertical-align:sub; font-size:small; '>3</span>R"
+
+-- Cardiac currents and intracellular concentrations
 INA = "I<span style='vertical-align:sub; font-size:small; '>Na</span>"
 IK = "I<span style='vertical-align:sub; font-size:small; '>K</span>"
 ICA = "I<span style='vertical-align:sub; font-size:small; '>Ca</span>"
 IFUNNY = "I<span style='vertical-align:sub; font-size:small; '>f</span>"
-NADP = "NADP<span style='vertical-align:super; font-size:small; '>+</span>"
-
 CAI = "[" .. CA .. "]<span style='vertical-align:sub; font-size:small; '>i</span>"
 CAMPI = "[cAMP]<span style='vertical-align:sub; font-size:small; '>i</span>"
 
+
+-- Spirometry
+FEV1 = "FEV<sub>1</sub>"
+FEV1P = "FEV<sub>1%</sub>"
+PACO2 = "P<sub>A</sub>" .. CO2
+PaCO2 = "P<sub>a</sub>" .. CO2
+PAO2 = "P<sub>A</sub>" .. O2
+
+-- Arrows
 -- Tried many other Unicode arrow symbols but they're ugly in some fonts
 DA = "<span style='font-weight:bold; color:blue; '>&darr;</span>"
 UA = "<span style='font-weight:bold; color:blue; '>&uarr;</span>"
 DDA = "<span style='font-weight:bold; color:blue; '>&#x21ca;</span>"
 UUA = "<span style='font-weight:bold; color:blue; '>&#x21c8;</span>"
 
--- A "P" in a circle, denoting phosphorylation
-PPN = "&#x024c5;" 
 
+-- Miscellaneous
 T4 = "&there4;"
 -- because (upside down therefore)
 BC = "&#x2235;"
@@ -67,9 +108,22 @@ BC = "&#x2235;"
 POS = "<span style='color:green; font-weight:bold; '>&oplus;</span>"
 NEG = "<span style='color:red; font-weight:bold; '>&#x02296;</span>"
 
--- There's no X-bar in Unicode; combine x and the bar instead.
--- FIXME: this doesn't work in some fonts -- why not?
-MEAN = "x&#772;"
+MALE = "&#x2642;"
+FEMALE = "&#x2640;"
+
+-- Maths
+  -- There's no X-bar in Unicode; combine x and the bar instead.
+  -- FIXME: this doesn't work in some fonts -- why not?
+  MEAN = "x&#772;"
+  -- Many choices, no clear best one.
+  APPROX = "&sim;"
+  APPROXEQ = "&#x2248;"
+
+-- 3'-5' etc
+PRIME = "&#x2032;"
+
+-- oC
+DEGC = "&deg;C"
 
 -- Interrobang
 IBANG = "&#x203D;"
@@ -82,11 +136,11 @@ SMILE = SMILEY
 BR = "<br />"
 
 -- Meaning, not presentation :)
-raised, raises, increased, high, higher, greater, elevated, raising, maximises, maximising =
-UA,     UA,     UA,        UA,   UA,     UA,      UA,       UA,      UA,        UA
+raised, raises, increased, increases, high, higher, greater, elevated, raising, maximises, maximising =
+UA,     UA,     UA,        UA,        UA,   UA,     UA,      UA,       UA,      UA,        UA
 
-lowered, lowers, lower, lowering, minimises, minimising, reduces, reducing, reduced, low, decreased, decreasing, depressed =
-DA,      DA,     DA,    DA,       DA,        DA,         DA,      DA,       DA,      DA,  DA,        DA,         DA
+lowered, lowers, lower, lowering, minimises, minimising, reduces, reducing, reduced, low, decreased, decreasing, decreases, depressed =
+DA,      DA,     DA,    DA,       DA,        DA,         DA,      DA,       DA,      DA,  DA,        DA,         DA,        DA
 
 inhibits, inhibited, inhibiting =
 NEG,      NEG,       NEG
